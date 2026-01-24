8f11edcca79421ce6578a3ff8a555d5c327bfee4
Priyagpt353 <priyagpt353@gmail.com>
2026-01-24 19:07:31 +0530

    updating api key

--- gemini/genai_ex1.py
+++ gemini/genai_ex1.py
@@ -1,9 +1,15 @@
 from google import genai
+import os
+from dotenv import load_dotenv
 
-client = genai.Client(api_key="AIzaSyA1MTB2L09d-Yf6JnF09ddSpniMhLaUIp4")
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="How does AI work?"
+    contents="How does AI work? Explain in 2-3 lines.",
 )
 print(response.text)
